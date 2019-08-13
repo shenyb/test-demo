2db1f0203edccb23f608934be7e210f61e989b73
shenyanbin <shenyanbin@weli.cn>
2019-08-13 17:59:34 +0800

    test

--- src/main/java/com/syb/part3/BST.java
+++ src/main/java/com/syb/part3/BST.java
@@ -13,12 +13,46 @@ public class BST<Key extends Comparable,Value> {
         private Value value;
         private Node left,right;
         private  int N;
+
+        public Node(Key key, Value value) {
+            this.key = key;
+            this.value = value;
+        }
     }
     public Value get(Key key){
-        return null;
-
+        return get(key,root);
     }
-    public void put(Key key,Value value){
+
+    private Value get(Key key, Node root) {
+        if(root==null){return null;}
+        int compare = root.key.compareTo(key);
+        if(compare==0){
+            return root.value;
+        }
+        if(compare>0){
+            return get(key,root.left);
+        }else{
+            return get(key,root.right);
+        }
+    }
+
+    public Node put(Node root,Key key,Value value){
+        if(root == null){return new Node(key,value);}
+        int compare = root.key.compareTo(key);
+        if(compare==0){
+            root.value = value;
+        }else if(compare>0){
+            Node r = put(root.left,key,value);
+            if(r!=null){
+                root.left = r;
+            }
+        }else {
+            Node r = put(root.right,key,value);
+            if(r!=null){
+                root.right = r;
+            }
+        }
+        return null;
 
     }
     public int size(){
@@ -26,5 +60,20 @@ public class BST<Key extends Comparable,Value> {
     }
     // max,min,floor,ceiling,select,rank,delete,deleteMin,deleteMax,keys
 
-
+    public Value getMin(){
+        if(root == null){return null;}
+        Node min = root;
+        while(min.left!=null){
+            min = min.left;
+        }
+        return min.value;
+    }
+    public Value getMax(){
+        if(root == null){return null;}
+        Node min = root;
+        while(min.right!=null){
+            min = min.right;
+        }
+        return min.value;
+    }
 }
